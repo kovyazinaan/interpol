af41442b19498db35c9c8992a50753ed93681dc4
mashkalovaviktoria <viktoria.mashkalova@yandex.ru>
2023-06-06 16:24:16 +0300

    dzzz

---
+++ dz.py
@@ -0,0 +1,7 @@
+
+age = int(input())
+if age <= 12: a = 'ребенок'
+elif 13 < age <= 17: a = "подросток"
+elif 18 < age <=64: a = 'взрослый'
+else: a = 'пожилой человек'
+print ("Значит, ты {}!".format(a))
